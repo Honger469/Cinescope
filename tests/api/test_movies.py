349a33481726b9c0b11087d6fb031962afd8e9c5
Honger469 <jaslan469@mail.ru>
2026-03-30 17:58:21 +0400

    р

--- tests/api/test_movies.py
+++ tests/api/test_movies.py
@@ -9,3 +9,34 @@ class TestMoviesAPI:
         response_data = response.json()
 
         # Проверки
+        # Проверяем, что ключ 'movies' есть
+        assert "movies" in response_data, "Ключ 'movies' отсутствует в ответе"
+
+        # Если хотим проверить хотя бы один фильм:
+        if response_data["movies"]:
+            assert "id" in response_data["movies"][0], "ID фильма отсутствует в ответе"
+        else:
+            print("Список фильмов пуст на этой странице")
+        assert "pageCount" in response_data, "Ключ 'pageCount' отсутствует в ответе"
+        assert "count" in response_data, "Ключ count отсутствует в ответе"
+        assert "page" in response_data, "Ключ page отсутствуют в ответе"
+        assert "pageSize" in response_data, "Ключ pageSize отсутствуют в ответе"
+
+
+class TestMoviesAPINegative:
+    @pytest.mark.parametrize("field, value", [
+        ("pageSize", "21"),  # некорректное количество страниц
+        ("page", 21)  # некорректный номер страницы
+    ])
+    def test_get_poster_negative(self, api_manager_movies: ApiManagerMovies, test_poster):
+        data = test_user
+
+        if value_register == "MISSING":
+            data.pop(field_register, None)  # удаляем ключ из словаря
+        else:
+            data[field_register] = value_register  # изменяем или оставляем None
+
+        print(f"\nНегативный тест. Проверка поля {field_register}={value_register}")
+
+        expected_status = 400  # Важно! Ожидаемый статус-код
+        api_manager_auth.auth_api.register_user(test_user, expected_status)
