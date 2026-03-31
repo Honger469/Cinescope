df664c8a4cf8cf5474f849e84d3c7d7cccb1af29
Honger469 <jaslan469@mail.ru>
2026-03-31 21:08:53 +0400

    Коммит

--- tests/api/test_payment.py
+++ tests/api/test_payment.py
@@ -10,7 +10,7 @@ class TestPaymentAPI:
 
     def test_get_user_payment(self, admin_api, api_manager_payment:ApiManagerPayment):
         """Позитивный тест на получение платежей пользователя."""
-        print("\nПозитивный тест. Создание, получение, удаление фильма")
+        print("\nПозитивный тест. Получение платежей пользователя")
         user_id = "0bfbe544-2f80-472f-af9b-b7986490a3d7"
         response = api_manager_payment.payment_api.get_user_id_payment(user_id)
         response_data = response.json()
