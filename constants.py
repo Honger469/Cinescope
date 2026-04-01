afc0690377eb4f6181dac1b3bb10d7845411504b
Honger469 <jaslan469@mail.ru>
2026-04-01 17:25:42 +0400

    Автотесты Кинескоп 4 модуль

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-# constants.py
+# constants.pyd
 BASE_URL_AUTH = "https://auth.dev-cinescope.coconutqa.ru"
 BASE_URL_MOVIES = "https://api.dev-cinescope.coconutqa.ru"
 BASE_URL_PAYMENT = "https://payment.dev-cinescope.coconutqa.ru"
