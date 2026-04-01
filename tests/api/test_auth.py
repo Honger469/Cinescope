f8c08f529f8b6aef1b52656277436d84ac34ac04
Honger469 <jaslan469@mail.ru>
2026-04-01 11:39:18 +0400

    Готово для МР

--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -75,7 +75,7 @@ class TestAuthNegative:
         ("email", "abc"),  # некорректный email
         ("email", ""),     # пустая строка
         ("password", "1"), # неверный пароль
-        ("password", "")  # пустая строка
+        ("password", "")  # пустая2строка
     ])
     def test_negative_auth(self, api_manager_auth: ApiManagerAuth, registered_user, field_auth, value_auth):
         # Авторизация пользователя
